ab6d8af58f06a4401f4ad7db88789714a4855720
aliiasad <alii.asad0519@gmail.com>
2026-07-29 14:18:07 +0500

    Solution 010-2 Added with void --> int*

--- 011-Sliding-Sum/solution2.cpp
+++ 011-Sliding-Sum/solution2.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 
-int* getInput(int& counter);
-void getWindowSize(int& windowSize, int counter);
-void slidingSum(int input[], int inputSize, int windowSize);
+int* getInput(int& inputSize);
+void getWindowSize(int& windowSize, int inputSize);
+int* slidingSum(int* input, int inputSize, int windowSize);
+void displayResult(int* result, int inputSize, int windowSize);
+void deleteStructure(int* arr);
 
 int main()  {
-    int input[] = {3,1,4,1,5,9,2};
-    slidingSum(input, 7, 3);
+    int inputSize = 0, windowSize = 0;;
+    int* input = getInput(inputSize);
+    getWindowSize(windowSize, inputSize);
+    int* result = slidingSum(input, 7, 3);
+    displayResult(result, inputSize, windowSize);
+    deleteStructure(input);
+    deleteStructure(result);
     return 0;
 }
 
-int* getInput() {
+int* getInput(int& inputSize) {
     std::cout << "Enter Elements sseparated by \"Enter\". Hit \"S\" to Stop." << std::endl;
-    int size = 10, counter = 0;
+    int size = 10;
     int* input = new int [size];
     int num;
     // lambda to ensure capacity
     auto ensureCapacity = [&]() {
-        if (counter + 1 >= size)    {
+        if (inputSize + 1 >= size)    {
             size *= 2;
             int* tempInput = new int [size];
-            for (int i = 0; i < counter; i++)
+            for (int i = 0; i < inputSize; i++)
                 tempInput[i] = input[i];
             delete[] input;
             input = tempInput;
@@ -28,7 +35,7 @@ int* getInput() {
     };
     while (true)    {
         if (std::cin >> num)    {
-           input[counter++] = num;
+           input[inputSize++] = num;
            ensureCapacity(); 
         }
         else {
@@ -41,32 +48,53 @@ int* getInput() {
                 std::cout << "Invalid Input! Try again." << std::endl;
         }
     }
+    // make exact length
+    int* exactLength = new int [inputSize];
+    for (int i = 0; i < inputSize; i++)
+        exactLength[i] = input[i];
+    delete[] input;
+    input = exactLength;
+    size = inputSize
+;
+    return input;
 }
 
-void getWindowSize(int& windowSize, int counter) {
+void getWindowSize(int& windowSize, int inputSize) {
     std::cout << "Enter Size of Window (<= Size of Input)" << std::endl;
-    std::cin >> windowSize;
     while (true) {
         std::cin >> windowSize;
-        if (windowSize > counter && windowSize <= 0)
+        if (windowSize <= inputSize && windowSize >= 0)
             break;
         else 
             std::cout << "Invalid Size! Try Again" << std::endl;
     }
 }
 
-void slidingSum(int input[], int inputSize, int windowSize)   {
+int* slidingSum(int* input, int inputSize, int windowSize)   {
+    int* movingSum = new int [inputSize - windowSize + 1];
+    int k = 0;
     int front = 0, back = 0;
     int sum = 0;
-    std::cout << "[";
     while (back < inputSize)    {
         while (back - front < windowSize)
             sum += input[back++];
-        if (back == inputSize)
-            std::cout << sum << "]";
-        else
-            std::cout << sum << ", ";
+        movingSum[k++] = sum;
         sum -= input[front++]; 
     }
+    return movingSum;
+}
+
+void displayResult(int* result, int inputSize, int windowSize)  {
+    std::cout << "[";
+    for (int i = 0; i < inputSize - windowSize + 1; i++)    {
+        if (i == inputSize - windowSize)
+            std::cout << result[i] << "]";
+        else
+            std::cout << result[i] << ", ";
+    }
     return;
+}
+
+void deleteStructure(int* arr)    {
+    delete[] arr;
 }
