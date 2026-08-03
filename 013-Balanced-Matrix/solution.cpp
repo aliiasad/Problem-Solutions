daf83a0b5b15b6efe0e17aaa9e672ab8f02e7e59
aliiasad <alii.asad0519@gmail.com>
2026-08-03 11:05:13 +0500

    getInput() for 2d Added

---
+++ 013-Balanced-Matrix/solution.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+char** getInput(int& rows);
+
+int main()  {
+    int rows = 0;
+    char** input = getInput(rows);
+    return 0;
+}
+
+char** getInput(int& rows)   {
+    std::cout << "Enter Elements Consecutively for each Row! Hit Enter for new Row!" << std::endl;
+    std::cout << "Hit Enter at an Empty Row to Stop!" << std::endl;
+
+    // setup for 2d array
+    int extendableCapacity = 2; 
+    rows = 0;
+    char** matrix = new char* [extendableCapacity];
+
+    int size, counter;
+    char* word = nullptr;
+
+    // lambdas for both inner and outer capacity
+    auto ensureInnerCapacity = [&]() {
+        if (counter >= size)    {
+            size *= 2;
+            char* newWord = new char [size];
+            for (int i = 0; i < counter; i++)
+                newWord[i] = word[i];
+            delete[] word;
+            word = newWord;
+        }
+    };
+    auto ensureOuterCapacity = [&]() {
+        if (rows >= extendableCapacity) {
+            extendableCapacity *= 2;
+            char** newMatrix = new char* [extendableCapacity];
+            for (int i = 0; i < rows; i++)
+                newMatrix[i] = matrix[i];
+            delete[] matrix;
+            matrix = newMatrix;
+        }
+    };
+    auto shrinkToExact = [&]() {
+        char* exactSize = new char [counter + 1];
+        for (int i = 0; i <= counter; i++)
+            exactSize[i] = word[i];
+        delete[] word;
+        word = exactSize;
+    };
+
+    while (true)    {
+        // setup for each row
+        size = 10, counter = 0;
+        word = new char [size];
+        word[counter] = '\0';
+        char ch;
+
+        while (std::cin.get(ch) && ch != '\n')  {
+            if (ch > '9' || ch < '0') {
+                std::cout << "Skipped Non-Digit Characters" << std::endl;
+                continue;
+            }
+            word[counter++] = ch;
+            ensureInnerCapacity();
+            word[counter] = '\0';
+        }
+        // check if empty row
+        if (counter == 0)   {
+            delete[] word;
+            break;
+        }
+        // ensure no leaks on each row
+        shrinkToExact();
+
+        // ensure rows are available
+        ensureOuterCapacity();
+
+        matrix[rows++] = word;
+    }
+    return matrix;
+}
